3336639a3c458ae0291890bbf791daf9dc539a80
Mikael Karon <mikael@karon.se>
2023-08-04 23:17:49 +0200

    wip: add decorator tests

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -1,10 +1,24 @@
 import fastify, { type FastifyInstance } from 'fastify'
 import { expectType } from 'tsd'
-import fastifyUser, { type AddAuthStrategyDecorator } from '../..'
+import fastifyUser, {
+  type AddAuthStrategyDecorator,
+  type CreateJWTSessionDecorator,
+  type CreateSessionDecorator,
+  type CreateWebhookSessionDecorator,
+  type ExtractUserDecorator
+} from '../..'
 
 const app: FastifyInstance = fastify()
 app.register(fastifyUser)
 app.register(async (instance) => {
   expectType<AddAuthStrategyDecorator>(instance.addAuthStrategy)
+
+  instance.get('/', async (request) => {
+    expectType<ExtractUserDecorator>(request.extractUser)
+    expectType<CreateSessionDecorator>(request.createSession)
+    expectType<CreateSessionDecorator>(request.createSession)
+    expectType<CreateJWTSessionDecorator>(request.createJWTSession)
+    expectType<CreateWebhookSessionDecorator>(request.createWebhookSession)
+  })
 })
 
